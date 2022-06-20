553ea66a04a9058df43a3f7fdb27669a62a61ac2
sem scheffel <91524697+sscheffel@users.noreply.github.com>
2022-06-20 10:19:04 +0200

    Update dashboardHJZ.java

--- Java Programma/dashboardHJZ.java
+++ Java Programma/dashboardHJZ.java
@@ -2,86 +2,148 @@
 
 <?import javafx.scene.chart.PieChart?>
 <?import javafx.scene.control.Button?>
+<?import javafx.scene.control.ChoiceBox?>
 <?import javafx.scene.control.Label?>
+<?import javafx.scene.control.TextArea?>
 <?import javafx.scene.control.TextField?>
 <?import javafx.scene.image.Image?>
 <?import javafx.scene.image.ImageView?>
 <?import javafx.scene.layout.Pane?>
 <?import javafx.scene.text.Font?>
 
-<Pane fx:id="pnGeneral" prefHeight="778.0" prefWidth="1258.0" xmlns="http://javafx.com/javafx/17" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.Controller">
+<Pane fx:id="pnGeneral" prefHeight="778.0" prefWidth="1258.0" xmlns="http://javafx.com/javafx/18" xmlns:fx="http://javafx.com/fxml/1" fx:controller="sample.Controller">
    <children>
-      <Pane fx:id="pnAlgemeen" layoutY="-3.0" prefHeight="778.0" prefWidth="1258.0">
+      <Pane fx:id="pnAlgemeen" layoutX="-1.0" layoutY="-2.0" prefHeight="778.0" prefWidth="1258.0">
          <children>
             <Button fx:id="btnSpijkermakerij" mnemonicParsing="false" onAction="#showSpijkemakerij" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Spijkermakerij" textFill="#fafafa" />
             <Button fx:id="btnKantoor" layoutX="140.0" mnemonicParsing="false" onAction="#showKantoor" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Kantoor" textFill="WHITE" />
             <Button fx:id="btnOverig" focusTraversable="false" layoutX="280.0" mnemonicParsing="false" onAction="#showOverige" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Overige" textFill="WHITE" />
             <Button fx:id="btnStoring" layoutX="420.0" mnemonicParsing="false" onAction="#showStoring" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Storingen" textFill="WHITE" />
-            <PieChart fx:id="PCAlgemeen" layoutY="63.0" legendSide="RIGHT" mouseTransparent="true" prefHeight="408.0" prefWidth="1258.0" title="productie" />
-            <Label layoutX="155.0" layoutY="472.0" prefHeight="30.0" prefWidth="243.0" text="aantal geproduceerd vandaag">
-               <font>
-                  <Font size="18.0" />
-               </font></Label>
-            <Label layoutX="941.0" layoutY="478.0" text="aantal geproduceerd deze week">
-               <font>
-                  <Font size="18.0" />
-               </font></Label>
-            <Label fx:id="lblProductiviteit" layoutX="168.0" layoutY="585.0" prefHeight="25.0" prefWidth="112.0" text="Label">
-               <font>
-                  <Font name="Arial Narrow" size="54.0" />
-               </font>
-            </Label>
-            <Label layoutX="158.0" layoutY="559.0" prefHeight="18.0" prefWidth="234.0" text="productiviteit vandaag">
-               <font>
-                  <Font size="18.0" />
-               </font></Label>
-            <Label layoutX="288.0" layoutY="589.0" text="\%">
-               <font>
-                  <Font name="Arial Narrow" size="54.0" />
-               </font>
-            </Label>
-            <Label fx:id="lblProductieVandaag" layoutX="168.0" layoutY="502.0" prefHeight="18.0" prefWidth="132.0" text="Label">
-               <font>
-                  <Font name="Arial Narrow" size="31.0" />
-               </font>
-            </Label>
-            <Label fx:id="lblProductieWeek" layoutX="977.0" layoutY="502.0" text="Label">
-               <font>
-                  <Font name="Arial Narrow" size="31.0" />
-               </font>
-            </Label>
-            <Label layoutX="949.0" layoutY="550.0" text="productiviteit van deze week">
-               <font>
-                  <Font size="18.0" />
-               </font>
-            </Label>
-            <Label fx:id="lblPercentageWeek" layoutX="977.0" layoutY="579.0" text="Label">
-               <font>
-                  <Font size="54.0" />
-               </font>
-            </Label>
-            <Label layoutX="1121.0" layoutY="584.0" text="\%">
-               <font>
-                  <Font name="Arial Narrow" size="54.0" />
-               </font>
-            </Label>
             <Button fx:id="btnOrders" layoutX="559.0" mnemonicParsing="false" onAction="#showOrders" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Orders" textFill="#fafafa" />
-            <ImageView fitHeight="150.0" fitWidth="200.0" layoutX="52.0" layoutY="662.0" pickOnBounds="true" preserveRatio="true">
+            <Button fx:id="btnRefresh" layoutX="1118.0" mnemonicParsing="false" onAction="#TestVerbinding" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Refresh" textFill="#fafafa" />
+            <Button fx:id="btnExcel" layoutX="699.0" mnemonicParsing="false" onAction="#showExcel" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Excel" textFill="#fafafa" />
+            <ChoiceBox fx:id="CbTijd" layoutX="14.0" layoutY="106.0" prefHeight="25.0" prefWidth="132.0" />
+            <ImageView fx:id="imgHJZ" fitHeight="150.0" fitWidth="200.0" layoutX="52.0" layoutY="662.0" pickOnBounds="true" preserveRatio="true">
                <image>
                   <Image url="@pictures/hjz.png" />
                </image>
             </ImageView>
-            <Button fx:id="btnRefresh" layoutX="1118.0" mnemonicParsing="false" onAction="#TestVerbinding" prefHeight="63.0" prefWidth="140.0" style="-fx-background-color: blue;" text="Refresh" textFill="#fafafa" />
-            <Label layoutX="559.0" layoutY="472.0" prefHeight="30.0" prefWidth="243.0" text="laatste foutmelding">
+            <PieChart fx:id="PCAlgemeenDag" layoutY="63.0" legendSide="RIGHT" mouseTransparent="true" prefHeight="408.0" prefWidth="1258.0" title="productie" />
+            <PieChart fx:id="PCAlgemeenWeek" layoutY="63.0" legendSide="RIGHT" mouseTransparent="true" prefHeight="408.0" prefWidth="1258.0" title="productie" visible="false" />
+            <PieChart fx:id="PCAlgemeenMaand" layoutY="63.0" legendSide="RIGHT" mouseTransparent="true" prefHeight="408.0" prefWidth="1258.0" title="productie" visible="false" />
+            <PieChart fx:id="PCAlgemeenJaar" layoutY="63.0" legendSide="RIGHT" mouseTransparent="true" prefHeight="408.0" prefWidth="1258.0" title="productie" visible="false" />
+            <Label fx:id="lblAantalGeproduceerdVandaag" layoutX="152.0" layoutY="473.0" prefHeight="30.0" prefWidth="243.0" text="aantal geproduceerd vandaag">
+               <font>
+                  <Font size="18.0" />
+               </font></Label>
+            <Label fx:id="lblAantalGeproduceerdDezeWeek" layoutX="153.0" layoutY="474.0" text="aantal geproduceerd deze week" visible="false">
+               <font>
+                  <Font size="18.0" />
+               </font></Label>
+            <Label fx:id="lblAantalGeproduceerdMaand" layoutX="155.0" layoutY="472.0" prefHeight="30.0" prefWidth="275.0" text="aantal geproduceerd deze maand" visible="false">
                <font>
                   <Font size="18.0" />
                </font>
             </Label>
-            <Label fx:id="lblLaatsteFoutmelding" layoutX="559.0" layoutY="502.0" prefHeight="50.0" prefWidth="297.0" text="Label">
+            <Label fx:id="lblAantalGeproduceerdJaar" layoutX="155.0" layoutY="472.0" prefHeight="30.0" prefWidth="275.0" text="aantal geproduceerd dit jaar" visible="false">
+               <font>
+                  <Font size="18.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductivieitVandaag" layoutX="826.0" layoutY="474.0" prefHeight="18.0" prefWidth="234.0" text="productiviteit vandaag">
+               <font>
+                  <Font size="18.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductiviteitVanDezeWeek" layoutX="829.0" layoutY="474.0" text="productiviteit van deze week" visible="false">
+               <font>
+                  <Font size="18.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductiviteitVanDezeMaand" layoutX="823.0" layoutY="475.0" text="productiviteit van deze maand" visible="false">
+               <font>
+                  <Font size="18.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductiviteitVanDitJaar" layoutX="822.0" layoutY="475.0" text="productiviteit van dit jaar" visible="false">
+               <font>
+                  <Font size="18.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductieVandaag" layoutX="155.0" layoutY="502.0" prefHeight="18.0" prefWidth="132.0" text="Label">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductieWeek" layoutX="155.0" layoutY="502.0" text="Label" visible="false">
                <font>
                   <Font name="Arial Narrow" size="31.0" />
                </font>
             </Label>
+            <Label fx:id="lblProductieMaand" layoutX="155.0" layoutY="502.0" text="Label" visible="false">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductieJaar" layoutX="155.0" layoutY="502.0" text="Label" visible="false">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblProductiviteit" layoutX="831.0" layoutY="502.0" prefHeight="46.0" prefWidth="121.0" text="Label">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblPercentageWeek" layoutX="831.0" layoutY="497.0" prefHeight="46.0" prefWidth="106.0" text="Label" visible="false">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblPercentageMaand" layoutX="831.0" layoutY="502.0" prefHeight="46.0" prefWidth="106.0" text="Label" visible="false">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblPercentageJaar" layoutX="831.0" layoutY="504.0" prefHeight="46.0" prefWidth="106.0" text="Label" visible="false">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="txtPercentageTeken1" layoutX="1008.0" layoutY="509.0" text="\%">
+               <font>
+                  <Font name="Arial Narrow" size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblLaatstFoutmelding" layoutX="484.0" layoutY="472.0" prefHeight="30.0" prefWidth="243.0" text="laatste foutmelding">
+               <font>
+                  <Font size="18.0" />
+               </font>
+            </Label>
+            <Label fx:id="lblLaatsteFoutmelding" layoutX="484.0" layoutY="502.0" prefHeight="50.0" prefWidth="275.0" text="Label" textAlignment="CENTER">
+               <font>
+                  <Font name="Arial Narrow" size="31.0" />
+               </font>
+            </Label>
+            <Label fx:id="LblPeriode" layoutX="40.0" layoutY="76.0" prefHeight="30.0" prefWidth="80.0" text="periode;" textAlignment="CENTER" />
+            <Label fx:id="lblProductieJaarM" layoutX="312.0" layoutY="504.0" text="Duizend." visible="false">
+               <font>
+                  <Font size="31.0" />
+               </font>
+            </Label>
+         </children>
+      </Pane>
+      <Pane fx:id="pnExcel" prefHeight="778.0" prefWidth="1258.0" visible="false">
+         <children>
+            <ImageView fitHeight="150.0" fitWidth="200.0" layoutX="14.0" layoutY="677.0" pickOnBounds="true" preserveRatio="true">
+               <image>
+                  <Image url="@pictures/hjz.png" />
+               </image>
+            </ImageView>
+            <Button fx:id="btnTerugExcel" layoutX="14.0" layoutY="364.0" mnemonicParsing="false" onAction="#terugExcel" style="-fx-background-color: blue;" text="Terug" textFill="WHITE" />
+            <TextArea fx:id="txtAInhoud" layoutX="93.0" layoutY="41.0" prefHeight="653.0" prefWidth="1113.0" />
+            <Button fx:id="btnBekijkExcel" layoutX="1038.0" layoutY="720.0" mnemonicParsing="false" onAction="#bekijkExcel" style="-fx-background-color: blue;" text="Bekijk Excel" textFill="WHITE" />
+            <Button fx:id="btnOpslaanInExcel" layoutX="1150.0" layoutY="720.0" mnemonicParsing="false" onAction="#OpslaanInExcel" style="-fx-background-color: blue;" text="Opslaan" textFill="WHITE" />
          </children>
       </Pane>
       <Pane fx:id="pnSpijkemakerij" prefHeight="778.0" prefWidth="1258.0" visible="false">
